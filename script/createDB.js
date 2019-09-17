a4613ac3f68d6a39e713e26584c2f46dc45f937b
pom <chaiprasertp@hotmail.com>
2019-09-16 23:18:45 -0400

    update DB tables

--- script/createDB.js
+++ script/createDB.js
@@ -13,7 +13,9 @@ db.query('USE ' + dbconfig.database);
 
 
 let sql = `CREATE TABLE IF NOT EXISTS users (
-	id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
 	email VARCHAR(100) NOT NULL UNIQUE,
 	password VARCHAR(255)
 );`;
@@ -21,10 +23,13 @@ let sql = `CREATE TABLE IF NOT EXISTS users (
 let sql2 = `CREATE TABLE IF NOT EXISTS contact (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	first_name VARCHAR(100),
-	last_name VARCHAR(100),
+    last_name VARCHAR(100),
+    middle_name VARCHAR(50),
 	phone_number VARCHAR(15),
-	email VARCHAR(255) NOT NULL UNIQUE,
-	favorite BOOLEAN DEFAULT FALSE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    address VARCHAR(200),
+    favorite BOOLEAN DEFAULT FALSE,
+    profile_url VARCHAR(1000),
     group_id INT,
     user_id INT,
 	FOREIGN KEY (user_id) REFERENCES users(id)
